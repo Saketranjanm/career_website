3bcc039c539b54fd745865f7e76f544e07bff9bf
Saketranjanm <59251375+Saketranjanm@users.noreply.github.com>
2024-07-07 18:03:56 +0000

    uodate

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-  return "hello jovian"
+def hello_home():
+  return render_template('Templates/home.html')
 
 
 if __name__ == "__main__":
